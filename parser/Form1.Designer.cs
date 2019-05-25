3b196fafc99c49efc5a1f7eac8515a0b03471194
KirillBondarBn <kirill.bondar007@gmail.com>
2019-05-25 18:51:20 +0700

    Oh my...

--- parser/Form1.Designer.cs
+++ parser/Form1.Designer.cs
@@ -31,8 +31,11 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
@@ -53,6 +56,7 @@
             // 
             // button1
             // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button1.Location = new System.Drawing.Point(12, 64);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(163, 41);
@@ -61,26 +65,66 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // pictureBox1
+            // label1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(418, 28);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(186, 131);
-            this.pictureBox1.TabIndex = 3;
-            this.pictureBox1.TabStop = false;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(182, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Общих друзей: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(182, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Друзей: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(182, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Подписчиков: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(182, 213);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Фотографий: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(182, 235);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Отметок: ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(778, 248);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(778, 267);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -91,7 +135,11 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- parser/Form1.cs
+++ parser/Form1.cs
@@ -15,11 +15,13 @@ namespace parser
     public partial class Form1 : Form
     {
         public Label lb;
+        public PictureBox pb;
 
         public Form1()
         {
             InitializeComponent();
         }
+
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -43,27 +45,168 @@ namespace parser
                 foreach (IWebElement friend in FrList)
                 {
                     var ff = friend.FindElement(By.CssSelector(".friends_field"));
-                    var name = ff.FindElement(By.TagName("a")).Text;
+                    var name = ff.FindElement(By.TagName("a"));
                     var photo = friend.FindElement(By.TagName("img")).GetAttribute("src");
 
-                    pictureBox1.ImageLocation = photo;
 
-                    this.Controls.Add(new Label()
+
+                    this.lb = new Label()
                     {
-                        Name = "labeel" + flag.ToString(),
-                        Location = new Point(x, y),
-                        BackColor = Color.Red,
-                        AutoSize = true,
-                        Text = name
-                    });
-                    y += 50;
+                        Name = "lbFriend" + flag.ToString(),
+                        Location = new Point(x, y + 90),
+                        AutoSize = false,
+                        Text = name.Text,
+                        Tag = name.GetAttribute("href"),
+                        Cursor = Cursors.Hand
+                    };
+
+                    this.pb = new PictureBox()
+                    {
+                        Name = "pbFriend" + flag.ToString(),
+                        Location = new Point(x, y - 20),
+                        ImageLocation = photo,
+                        Width = 100,
+                        Height = 100,
+                        SizeMode = PictureBoxSizeMode.Zoom,
+                        Tag = name.GetAttribute("href"),
+                        Cursor = Cursors.Hand
+                    };
+                    Controls.Add(this.lb);
+                    Controls.Add(this.pb);
+
+                    lb.Click += label_Click;
+                    pb.Click += pictureBox_Click;
+
+                    x += 100;
                     flag++;
-                    if (flag == 13) break;
+                    if (flag == 5) break;
                 }
             }
             catch(Exception exp)
             {
                 MessageBox.Show(exp.ToString());
+            }
+            finally
+            {
+                driver.Quit();
+            }
+        }
+
+        private void label_Click(object sender, EventArgs e)
+        {
+            IWebDriver driver = new ChromeDriver();
+            try
+            {
+                string pass = textBox2.Text,
+                          log = textBox1.Text;
+
+                driver.Url = "https://vk.com";
+                driver.FindElement(By.Id("index_email")).SendKeys(log);
+                driver.FindElement(By.XPath(".//div[@id='index_login']/form/input[@id='index_pass']")).SendKeys(pass);
+                driver.FindElement(By.XPath(".//div[@id='index_login']/form/button[@id='index_login_button']")).Click();
+
+                this.lb = (sender as Label);
+                driver.Url = this.lb.Tag.ToString();
+
+                var CountModule = driver.FindElement(By.XPath(".//div[@class='counts_module']"));
+                var allCounts = CountModule.FindElements(By.ClassName("page_counter"));
+
+                int k = 0;
+                List<string> res = new List<string>();
+                foreach (IWebElement abc in allCounts)
+                {
+                    char[] txt = abc.Text.ToCharArray();
+                    
+                    res.Add("");
+
+                    for(int i = 0; i < txt.Length; i++)
+                    {
+                        if (txt[i] != '\r' && txt[i] != '\n') res[k] += txt[i].ToString();
+                        else break;
+                    }
+
+                    k++;
+                }
+                if (res[0] != "") label1.Text = "Общих друзей: " + res[0];
+                else label1.Text = "Общих друзей: 0 (либо данные скрыты)";
+                
+                if (res[1] != "") label2.Text = "Друзей: " + res[1];
+                else label2.Text = "Друзей: 0 (либо данные скрыты)";
+                
+                if (res[2] != "") label3.Text = "Подписчиков: " + res[2];
+                else label3.Text = "Подписчиков: 0 (либо данные скрыты)";
+                
+                if (res[3] != "") label4.Text = "Фотографий: " + res[3];
+                else label4.Text = "Фотографий: 0 (либо данные скрыты)";
+                
+                if (res[4] != "") label5.Text = "Отметок: " + res[4];
+                else label5.Text = "Отметок: 0 (либо данные скрыты)";
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.ToString());
+            }
+            finally
+            {
+            }
+        }
+
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+            IWebDriver driver = new ChromeDriver();
+            try
+            {
+                string pass = textBox2.Text,
+                          log = textBox1.Text;
+
+                driver.Url = "https://vk.com";
+                driver.FindElement(By.Id("index_email")).SendKeys(log);
+                driver.FindElement(By.XPath(".//div[@id='index_login']/form/input[@id='index_pass']")).SendKeys(pass);
+                driver.FindElement(By.XPath(".//div[@id='index_login']/form/button[@id='index_login_button']")).Click();
+
+
+                this.pb = (sender as PictureBox);
+                driver.Url = this.pb.Tag.ToString();
+
+                var CountModule = driver.FindElement(By.XPath(".//div[@class='counts_module']"));
+                var allCounts = CountModule.FindElements(By.ClassName("page_counter"));
+
+                int k = 0;
+                List<string> res = new List<string>();
+                foreach (IWebElement abc in allCounts)
+                {
+                    char[] txt = abc.Text.ToCharArray();
+                    res.Add("");
+
+                    for (int i = 0; i < txt.Length; i++)
+                    {
+                        if (txt[i] != '\r' && txt[i] != '\n') res[k] += txt[i].ToString();
+                        else break;
+                    }
+
+                    k++;
+                }
+                if (res[0] != "") label1.Text = "Общих друзей: " + res[0];
+                else label1.Text = "Общих друзей: 0 (либо данные скрыты)";
+
+                if (res[1] != "") label2.Text = "Друзей: " + res[1];
+                else label2.Text = "Друзей: 0 (либо данные скрыты)";
+
+                if (res[2] != "") label3.Text = "Подписчиков: " + res[2];
+                else label3.Text = "Подписчиков: 0 (либо данные скрыты)";
+
+                if (res[3] != "") label4.Text = "Фотографий: " + res[3];
+                else label4.Text = "Фотографий: 0 (либо данные скрыты)";
+
+                if (res[4] != "") label5.Text = "Отметок: " + res[4];
+                else label5.Text = "Отметок: 0 (либо данные скрыты)";
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.ToString());
+            }
+            finally
+            {
                 driver.Quit();
             }
         }
